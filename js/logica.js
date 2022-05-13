3bd65c9c02f61416aca83e9d4b243f6284c4a522
Alberto N.R <nochebeto12@gmail.com>
2022-05-13 11:58:29 -0500

    modificaciones al mp3

---
+++ js/logica.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+   buidInterfaz();
+}
+
+var getAlbums = () =>  ['song1.mp3', 'song2.mp3', 'song3.mp3', 'song4.mp3'];
+
+function buidInterfaz() {
+
+   const albums = getAlbums();
+
+
+
+}
