c7abea8887ad131f62a5c30fce462bd17b56a90d
Alberto N.R <nochebeto12@gmail.com>
2022-05-14 08:18:48 -0500

    modificaciones al mp3

--- js/logica.js
+++ js/logica.js
@@ -2,7 +2,9 @@ window.onload = function () {
    buidInterfaz();
 }
 
-var getAlbums = () =>  ['song1.mp3', 'song2.mp3', 'song3.mp3', 'song4.mp3'];
+
+
+var getSourceMusic = () =>  ['song1.mp3', 'song2.mp3', 'song3.mp3', 'song4.mp3'];
 
 function buidInterfaz() {
 
---
+++ js/player.js
@@ -0,0 +1,18 @@
+loadDataJSONfile();
+initPlayer();
+
+
+function initPlayer() {
+
+    var audio = document.getElementById("audio");
+
+    // audio.src = "../resources/music/The Neighbourhood - Devil's Advocate.mp3";
+    //
+    // audio.load();
+    // audio.play();
+}
+
+function loadDataJSONfile() {
+
+
+}
